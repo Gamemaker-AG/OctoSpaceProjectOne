3116225858b5a2e9fc0654643edfde6b3a5279cd
14rau <patrickrau@kabelmail.de>
2014-12-09 02:19:05 +0100

    Skripts, 3dModells and prefabs
    Weapon Skripts, Energie Skripts, Shield Skript, Some Prefabs. Edited
    some Options in the Inputmanager.

---
+++ Unity/Assets/Scripts/Waffe_Projektil.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+
+public class Waffe_Projektil : MonoBehaviour {
+
+	public float speed = 1f;
+	public float DetoTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, DetoTime);
+	}
+
+	void FixedUpdate()
+
+	{
+		bewegeProjektil ();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Enemy")
+		{
+			Destroy (gameObject);
+		}
+
+
+	}
+
+	void bewegeProjektil(){
+		transform.Translate (0f, 0f, speed * Time.deltaTime);
+
+		}
+
+}
